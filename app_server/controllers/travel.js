086b9f5a26996a4ecc5ac700665a817fb703599d
Brent Meraviglia <brentmeraviglia@gmail.com>
2024-11-24 02:31:32 -0500

    Module 3 Completed Baseline

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,3 +1,9 @@
+/* Edit the travel.js controller file to use the built-in Node.js file system component to read the
+data file that you just created. Use the fs.readFileSync() method to retrieve the JSON. */
+
+var fs = require('fs');
+var trips = JSON.parse(fs.readFileSync('./data/trips.json','utf8'));
+
 /* GET travel view */
 const travel = (req, res) => {
     res.render('travel', { title: 'Travlr Getaways'});
