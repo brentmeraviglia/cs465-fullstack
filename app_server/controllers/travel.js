a59883e7b768d1d64fafdebd61851a401545bb00
Brent Meraviglia <brentmeraviglia@gmail.com>
2024-12-08 23:18:16 -0500

    Module 5 completed baseline

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,12 +1,34 @@
-/* Edit the travel.js controller file to use the built-in Node.js file system component to read the
-data file that you just created. Use the fs.readFileSync() method to retrieve the JSON. */
+const tripsEndpoint = 'http://localhost:3000/api/trips';
+const options = {
+    method: 'GET',
+    headers: {
+        'Accept': 'application/json'
+    }
+}
 
-var fs = require('fs');
-var trips = JSON.parse(fs.readFileSync('./data/trips.json','utf8'));
+// var fs = require('fs');
+// var trips = JSON.parse(fs.readFileSync('./data/trips.json','utf8'));
 
 /* GET travel view */
-const travel = (req, res) => {
-    res.render('travel', { title: 'Travlr Getaways'});
+const travel = async function(req, res, next) {
+    // console.log('TRAVEL CONTROLLER BEGIN');
+    await fetch(tripsEndpoint, options)
+        .then(res => res.json())
+        .then(json => {
+            // console.log(json);
+            let message = null;
+            if(!(json instanceof Array)) {
+                message = 'API lookup error';
+                json = [];
+            } else {
+                if(!json.length){
+                    message = 'No trips exist in our database!';
+                }
+            }
+            res.render('travel', {title: 'Travlr Getaways', trips: json});
+        })
+        .catch(err => res.status(500).send(e.message));
+    // console.log('TRAVEL CONTROLLER AFTER RENDER');
 };
 
 module.exports = {
